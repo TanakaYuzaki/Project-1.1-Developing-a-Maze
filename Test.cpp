889179d0c385b7648cc1bcfe3880e5ab866b9050
Duy Khang <131791276+TanakaYuzaki@users.noreply.github.com>
2025-10-31 13:49:06 +0700

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+double Solution(string Original, string Received){
+    
+
+}
+
+int main(){
+    string Original, Received;
+    cin>>Original;
+    cin>>Received;
+    double res = Solution(Original, Received);
+
+}
